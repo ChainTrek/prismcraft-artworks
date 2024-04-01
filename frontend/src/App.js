5d7161eafb619d3c04ca664b26096b4d0095b33c
Elijah Cravens <elijahcravens46@gmail.com>
2024-04-01 11:25:52 -0600

    Add Bootstrap, React Bootstrap, and React Icons dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
-	return <h1>Welcome to Prism Craft Artworks</h1>
+	return (
+		<>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome to PrismCraft Artworks</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
+	)
 }
 
 export default App
