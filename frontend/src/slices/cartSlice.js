f1afb798a0d3b12440ab9c7f60416a48bac07e9e
Elijah Cravens <elijahcravens46@gmail.com>
2024-04-11 11:23:38 -0600

    Refactor ProductScreen component to use Redux for state management and improve user experience

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -12,7 +12,7 @@ const cartSlice = createSlice({
 	reducers: {
 		addToCart: (state, action) => {
 			const item = action.payload
-			const existItem = state.cartItems.find(x => x.product === item.product)
+			const existItem = state.cartItems.find(x => x._id === item._id)
 			if (existItem) {
 				state.cartItems = state.cartItems.map(x => (x._id === existItem._id ? item : x))
 			} else {
